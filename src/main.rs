00f008fc16b10516e9d750456a1fda83a26404db
Zephyr Shannon <earthlingzephyr@gmail.com>
2021-05-23 18:56:41 -0700

    Add handling for interpreter errors

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 
 use anyhow::Context;
 use structopt::StructOpt;
+use thiserror::Error;
 
 #[derive(StructOpt)]
 struct Args {
@@ -21,24 +22,51 @@ fn main(args: Args) -> anyhow::Result<()> {
     }
 }
 
+#[derive(Debug, Error)]
+#[error("[line {line_number}] Error{location}: {error}")]
+struct Error {
+    line_number: usize,
+    location: String,
+    error: anyhow::Error,
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 fn run_prompt() -> anyhow::Result<()> {
     let stdin = io::stdin();
-    run_all(stdin.lock().lines()).context("from standard input")
+    loop {
+        let result = run_all(stdin.lock().lines()).context("from standard input");
+        print_interpreter_error(result)?;
+    }
 }
 
 fn run_file(script: PathBuf) -> anyhow::Result<()> {
     let buf_reader = BufReader::new(File::open(script).context("")?);
-    run_all(buf_reader.lines()).context("from file")
+    let result = run_all(buf_reader.lines());
+    print_interpreter_error(result).context("from file")
 }
 
-fn run_all(lines: impl Iterator<Item = io::Result<String>>) -> anyhow::Result<()> {
+fn print_interpreter_error(error: anyhow::Result<Result<()>>) -> anyhow::Result<()> {
+    match error {
+        Ok(Ok(())) => Ok(()),
+        Ok(Err(interpreter_error)) => {
+            eprintln!("{}", interpreter_error);
+            Ok(())
+        }
+        Err(io_error) => Err(io_error)?,
+    }
+}
+
+fn run_all(lines: impl Iterator<Item = io::Result<String>>) -> anyhow::Result<Result<()>> {
     for line_res in lines {
         let line = line_res.context("failed to read line")?;
-        run(line)?;
+        if let Err(interpreter_error) = run(line) {
+            return Ok(Err(interpreter_error));
+        }
     }
-    Ok(())
+    Ok(Ok(()))
 }
 
-fn run(_code: String) -> anyhow::Result<()> {
+fn run(_code: String) -> Result<()> {
     todo!("actually run")
 }
